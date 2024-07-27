8b45339f43c2109d6020d0c2fad5ceb75714b3fb
pgarbrecht <philgarbrecht@gmail.com>
2024-07-27 11:17:11 -0500

    setup: adds routes folder structure and gets test  route working
    Signed-off-by: pgarbrecht <philgarbrecht@gmail.com>

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
+// Dependencies
 import Express from 'express';
+import userRoutes from './routes/users.js';
+
+// Variables
 const app = Express();
 const port = 3001;
+
+// Routing
+app.use('/api/users', userRoutes);
 
 app.listen(port, () => {
 	console.log(`Server is running at port: ${port}`);
