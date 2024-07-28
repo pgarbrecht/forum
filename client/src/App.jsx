cc294d527eaa3013875ff355ef0f2f6925c548f9
pgarbrecht <philgarbrecht@gmail.com>
2024-07-28 12:28:09 -0500

    setup: adds routing for home, register, login
    Signed-off-by: pgarbrecht <philgarbrecht@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 
 function App() {
 	return (
-		<>
-			<h1>The Forum</h1>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/register' element={<Register />} />
+				<Route path='/login' element={<Login />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
