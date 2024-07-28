6646f106bddaeea1c50377d1414970cf6323ca6e
pgarbrecht <philgarbrecht@gmail.com>
2024-07-28 14:54:04 -0500

    style: designs the login page
    Signed-off-by: pgarbrecht <philgarbrecht@gmail.com>

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -3,9 +3,28 @@ import './login.scss';
 
 const Login = () => {
 	return (
-		<>
-			<div className='login'>Login</div>
-		</>
+		<div className='login'>
+			<div className='card'>
+				<div className='left'>
+					<h1>Welcome to the Forum</h1>
+					<p>
+						This is a place for great minds to discuss great topics. Bring your
+						deep questions, curiosities, and musings here. Where there is an
+						open mind, there will always be a great frontier to explore.
+					</p>
+					<span>Don't have an account?</span>
+					<button>Register</button>
+				</div>
+				<div className='right'>
+					<h2>Login</h2>
+					<form>
+						<input type='text' placeholder='Email' />
+						<input type='password' placeholder='Password' />
+						<button>Submit</button>
+					</form>
+				</div>
+			</div>
+		</div>
 	);
 };
 
